58b1ad2cdffb96a795d915ddbf7d9a3c91caea2f
Lijo <lijo.belardi@icloud.com>
2022-06-20 13:51:43 +0200

    added AllDataSection's basic style

--- src/components/AllDataSection/index.jsx
+++ src/components/AllDataSection/index.jsx
@@ -1,12 +1,20 @@
-import React, {useContext} from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import styles from './index.module.scss' 
+import apiClient from '../../apiClient'
 import { AllGlobalDataContext } from '../../context/AllGlobalData'
+import logErrors from '../../utility/consoleShortcuts'
+import {Container, Typography} from '@mui/material'
 
 const AllDataSection = () => {
     const [data, setData] = useContext(AllGlobalDataContext)
     console.log(data);
-    
+
     return (
-        <div>AllDataSection</div>
+        <Container className={styles.globalDataContainer}>
+            <Typography variant='h4'>Global Data</Typography>
+            <p>{data.cases}</p>
+
+        </Container>
     )
 }
 
