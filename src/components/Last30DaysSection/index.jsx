1d6d9c6753b697745454fe6580b5cf7774244093
Lijo <lijo.belardi@icloud.com>
2022-06-21 01:46:35 +0200

    Defined basic Linechart for 30Days's section

--- src/components/Last30DaysSection/index.jsx
+++ src/components/Last30DaysSection/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+// My components
+import LineChartAllLast30DaysCases from '../LineChart/Last30Days/Cases'
+import LineChartAllLast30DaysDeaths from '../LineChart/Last30Days/Deaths'
+
 // Mui components
-import { Container, Typography, Grid } from '@mui/material'
+import { Container, Typography, Grid, Card } from '@mui/material'
 // Style
 import styles from './index.module.scss'
 
@@ -8,6 +12,19 @@ const Last30DaysSection = () => {
     return (
         <Container className={styles.Last30DaysContainer}>
             <Typography variant='h3' align='center'>Last 30 days</Typography>
+
+            <Grid container spacing={4}>
+                <Grid item lg={12}>
+                    <Card>
+                        <LineChartAllLast30DaysCases />
+                    </Card>
+                </Grid>
+                <Grid item lg={12}>
+                    <Card>
+                        <LineChartAllLast30DaysDeaths />
+                    </Card>
+                </Grid>
+            </Grid>
         </Container>
     )
 }
---
+++ src/components/LineChart/Last30Days/Deaths/index.jsx
@@ -0,0 +1,48 @@
+// React
+import React, { useState, useEffect } from 'react'
+// React-Chartjs-2
+import { Line } from 'react-chartjs-2';
+// Chart.js
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement } from 'chart.js';
+// Others import
+import apiClient from '../../../../apiClient'
+import logErrors from '../../../../utility/consoleShortcuts'
+
+
+// ChartJs Register
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement);
+
+const LineChartAllLast30DaysDeaths = () => {
+    const [deaths, setDeaths] = useState({})
+    let date = Object.keys(deaths)
+    let deathsDateValue = Object.values(deaths)
+
+    const data = {
+        labels: date.map(day => day),
+        datasets: [
+            {
+                label: 'Deaths',
+                backgroundColor: '#AE3813',
+                borderColor: '#AE3813',
+                data: deathsDateValue.map(singleValue => singleValue),
+            }
+        ]
+    };
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const getData = async () => {
+        try {
+            const response = await apiClient.get('/v3/covid-19/historical/all?lastdays=30')            
+            setDeaths(response.data.deaths)
+        } catch (error) {
+            logErrors(error)
+        }
+    }
+
+    return (<Line data={data} style={{ maxHeight: 400 }} />)
+}
+
+export default LineChartAllLast30DaysDeaths
