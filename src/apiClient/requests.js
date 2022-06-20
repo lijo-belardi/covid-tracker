73807d6e3206f9dd8d7ddfc3ba32157d3ac305c5
Lijo <lijo.belardi@icloud.com>
2022-06-21 01:02:33 +0200

    Added new request

--- src/apiClient/requests.js
+++ src/apiClient/requests.js
@@ -1,5 +1,6 @@
 const requests = {
     allGlobalData: `${process.env.REACT_APP_BASE_URL}/v3/covid-19/all?allowNull=0`,
+    last30DaysData: `${process.env.REACT_APP_BASE_URL}/v3/covid-19/historical/all?lastdays=30`,
 }
 
 export default requests
