30c79965851b79b59d24cd9b8129bcd6e27d916c
Lijo <lijo.belardi@icloud.com>
2022-06-21 01:02:48 +0200

    Changed title's align

--- src/components/Last30DaysSection/index.jsx
+++ src/components/Last30DaysSection/index.jsx
@@ -4,12 +4,12 @@ import { Container, Typography, Grid } from '@mui/material'
 // Style
 import styles from './index.module.scss'
 
-const Last30DaysSection = () => {
-  return (
-    <Container className={styles.Last30DaysContainer}>
-        <Typography variant='h3' align='left'>Last 30 days</Typography>
-    </Container>
-  )
+const Last30DaysSection = () => { 
+    return (
+        <Container className={styles.Last30DaysContainer}>
+            <Typography variant='h3' align='center'>Last 30 days</Typography>
+        </Container>
+    )
 }
 
 export default Last30DaysSection
