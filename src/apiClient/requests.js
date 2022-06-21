0c06510f823d7c22abe9e69a0eff28d5aa3ed5a1
Lijo <lijo.belardi@icloud.com>
2022-06-21 10:21:57 +0200

    Added continent's request

--- src/apiClient/requests.js
+++ src/apiClient/requests.js
@@ -1,6 +1,7 @@
 const requests = {
     allGlobalData: `${process.env.REACT_APP_BASE_URL}/v3/covid-19/all?allowNull=0`,
     last30DaysData: `${process.env.REACT_APP_BASE_URL}/v3/covid-19/historical/all?lastdays=30`,
+    continentsData: `${process.env.REACT_APP_BASE_URL}/v3/covid-19/continents`
 }
 
 export default requests
