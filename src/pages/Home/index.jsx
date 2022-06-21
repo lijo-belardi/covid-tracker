7ac1603ce4b90214bdc39d3ce1049a4f0f0143b6
Lijo <lijo.belardi@icloud.com>
2022-06-21 14:44:43 +0200

    Created basic Footer

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
+// components
 import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+// components/Sections
 import ErrorBoundary from '../../components/ErrorBoundary'
 import AllDataSection from '../../components/Sections/AllDataSection'
 import ContinentsSection from '../../components/Sections/ContinentsSection'
@@ -34,7 +37,9 @@ const Home = () => {
 
       
       {/* TODO Footer */}
-
+      <ErrorBoundary>
+        <Footer />
+      </ErrorBoundary>
     </div>
   )
 }
