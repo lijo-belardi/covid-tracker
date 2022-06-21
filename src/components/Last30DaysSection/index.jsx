e282c0e93fdb55902ac1a1b085c9067273d63093
Lijo <lijo.belardi@icloud.com>
2022-06-21 10:22:54 +0200

    Changed file's position

--- src/components/Last30DaysSection/index.jsx
+++ src/components/Last30DaysSection/index.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 // My components
-import LineChartAllLast30DaysCases from '../LineChart/Last30Days/Cases'
-import LineChartAllLast30DaysDeaths from '../LineChart/Last30Days/Deaths'
+import LineChartAllLast30DaysCases from '../Chart/LineChart/Last30Days/Cases'
+import LineChartAllLast30DaysDeaths from '../Chart/LineChart/Last30Days/Deaths'
 
 // Mui components
 import { Container, Typography, Grid, Card } from '@mui/material'
 // Style
 import styles from './index.module.scss'
 
-const Last30DaysSection = () => { 
+const Last30DaysSection = () => {
     return (
         <Container className={styles.Last30DaysContainer}>
             <Typography variant='h3' align='center'>Last 30 days</Typography>
 
+            {/* TODO migliorare vista griglia */}
+            {/* TODO testare responsività */}
             <Grid container spacing={4}>
                 <Grid item lg={12}>
                     <Card>
