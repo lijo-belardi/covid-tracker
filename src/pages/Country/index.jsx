44df4606684df79e7bac925ceadd537ca8d8cf47
Lijo <lijo.belardi@icloud.com>
2022-06-22 08:23:10 +0200

    Added basic info

--- src/pages/Country/index.jsx
+++ src/pages/Country/index.jsx
@@ -1,11 +1,16 @@
 // React
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+// My Components
+import Header from '../../components/Header'
+import CardInfo from '../../components/CardInfo'
 // Mui Components
-import { Typography } from '@mui/material'
+import { Typography, Container, Grid, Card } from '@mui/material'
 // Others import 
 import apiClient from '../../apiClient'
-
+import logErrors from '../../utility/consoleShortcuts'
+// Styles
+import styles from './index.module.scss'
 
 const Country = () => {
     let params = useParams()
@@ -21,12 +26,44 @@ const Country = () => {
             console.log(response.data);
             setCountry(response.data)
         } catch (error) {
-            console.log(error.message);
+            logErrors(error)
         }
     }
+
     return (
         <div>
-            <Typography variant='h3'>{country.country} - Covid Situation</Typography>
+            <Header />
+            <Container className={styles.titleContainer}>
+                <Typography variant='h3'>{country.country} - Covid Situation</Typography>
+            </Container>
+
+            <Container>
+                <Grid container spacing={4}>
+                    <Grid item xs={12} sm={4}>
+                        <CardInfo title='Cases' data={country.cases} todayData={country.todayCases} />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <CardInfo title='Deaths' data={country.deaths} todayData={country.todayDeaths} />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <CardInfo title='Recovered' data={country.recovered} todayData={country.todayRecovered} />
+                    </Grid>
+                </Grid>
+            </Container>
+
+            <Container className={styles.otherInfo}>
+                <Card className={styles.otherInfoCard}>
+                    <Typography variant='h4'>Other Information</Typography>
+                    <Typography variant='h6'>
+                        Continent: {country.continent}
+                    </Typography>
+
+                    <Typography variant='h6'>
+                        Population: {country.population}
+                    </Typography>
+                </Card>
+            </Container>
+
         </div>
     )
 }
