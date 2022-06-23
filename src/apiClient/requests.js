dd5eb2b64e9e6ab1f1da3b7728090c2e112b4195
Lijo <lijo.belardi@icloud.com>
2022-06-23 17:54:38 +0200

    Defined USA's page and added millify

--- src/apiClient/requests.js
+++ src/apiClient/requests.js
@@ -3,6 +3,7 @@ const requests = {
     last30DaysData: '/v3/covid-19/historical/all?lastdays=30',
     continentsData: '/v3/covid-19/continents',
     countriesCases: '/v3/covid-19/countries?sort=cases',
+    usaData: '/v3/covid-19/countries/usa?strict=true',
 }
 export default requests
 
--- src/pages/USA/index.jsx
+++ src/pages/USA/index.jsx
@@ -1,8 +1,53 @@
+// React
 import React from 'react'
+// My Components
+import Header from '../../components/Header'
+import CardInfo from '../../components/CardInfo'
+// MUI Components
+import { Container, Grid } from '@mui/material'
+// Others import
+import useApiClient from '../../hooks/useApiClient'
+import requests from '../../apiClient/requests'
+// Styles
+import styles from './index.module.scss'
+import millify from 'millify'
 
 const USAPage = () => {
+  const { data: country, loading, error } = useApiClient(requests.usaData)
+  console.log(country);
   return (
-    <div>USAPage</div>
+    /* TODO aggiungere Typograhy */
+    <div>
+      <Header />
+      <Container className={styles.titleContainer}>
+        {country && <h1>{country.country}</h1>}
+      </Container>
+
+      <Container>
+        {country && <Grid container spacing={4}>
+          <Grid item xs={12} sm={4}>
+            <CardInfo
+              title='Cases'
+              data={millify(country.cases)}
+              todayData={country.todayCases} />
+          </Grid>
+
+          <Grid item xs={12} sm={4}>
+            <CardInfo
+              title='Deaths'
+              data={millify(country.deaths)}
+              todayData={country.todayDeaths} />
+          </Grid>
+
+          <Grid item xs={12} sm={4}>
+            <CardInfo
+              title='Recovered'
+              data={millify(country.recovered)}
+              todayData={country.todayRecovered} />
+          </Grid>
+        </Grid>}
+      </Container>
+    </div>
   )
 }
 
