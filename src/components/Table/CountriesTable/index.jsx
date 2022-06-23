d0ec3d1c4f28adf8614757a441abb8fa923dbf29
Lijo <lijo.belardi@icloud.com>
2022-06-23 20:56:33 +0200

    Deleted TODO

--- src/components/Table/CountriesTable/index.jsx
+++ src/components/Table/CountriesTable/index.jsx
@@ -19,7 +19,6 @@ import logErrors from '../../../utility/consoleShortcuts'
 
 const CountriesTable = () => {
     const [countries, seCountries] = useState([])
-
     useEffect(() => {
         getData()
     }, [])
@@ -56,7 +55,6 @@ const CountriesTable = () => {
                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                             >
                                 <TableCell component="th" scope="row">
-                                    {/* TODO riguardare per aggiungere il Link */}
                                     <Link to={`/countries/${country.country}`}>
                                     {country.country}
                                     </Link>
