dac542b8ab5d6314b83b610400296b4f87ac4f04
Lijo <lijo.belardi@icloud.com>
2022-06-23 13:35:24 +0200

    fixed empty array

--- src/pages/Continents/index.jsx
+++ src/pages/Continents/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import useApiClient from '../../hooks/useApiClient'
 
 
 /* TODO completare */
 
 const Continents = () => {
+  const { data: continents, loading } = useApiClient('/v3/covid-19/continents')
+
   return (
-    <div>ContinentsPage</div>
+    <div>
+      {continents &&
+        continents.map(continent => <h1 key={continent.continent}>{continent.continent}</h1>)
+      }
+    </div>
   )
 }
 
