94982733c0f80707be5adbcff6e1805b87f8ca14
Lijo <lijo.belardi@icloud.com>
2022-06-25 16:32:24 +0200

    Changed the Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,72 @@
-import React from 'react'
-import Navbar from '../Navbar'
-//TODO risolvere lo stile
-//import styles from './index.module.scss' 
+// React 
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+// Mui Components
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+
 const Header = () => {
+  const navigate = useNavigate()
+  const [anchorEl, setAnchorEl] = useState(null);
+  
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClick = (pageURL) => {
+    navigate(pageURL)
+  };
+
   return (
-    <header>
-        
-            <Navbar />
-        
-    </header>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Photos
+          </Typography>
+
+          <div>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorEl)}
+              onClose={() => setAnchorEl(null)}
+            >
+              <MenuItem onClick={() => handleMenuClick('/')}>Home</MenuItem>
+              <MenuItem onClick={() => handleMenuClick('/countries/usa')}>USA - Page</MenuItem>
+              <MenuItem onClick={() => handleMenuClick('/countries/italy')}>Italy - Page</MenuItem>
+              <MenuItem onClick={() => handleMenuClick('/continents')}>Continents</MenuItem>
+            </Menu>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
 export default Header
