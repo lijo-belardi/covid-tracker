ee424131ed80cd415338acf86424bdf2b52070b3
Lijo <lijo.belardi@icloud.com>
2022-06-26 00:24:22 +0200

    Changed menuItems order

--- src/components/Header/menuItems.js
+++ src/components/Header/menuItems.js
@@ -1,8 +1,8 @@
 const menuItems = [
     { menuTitle: 'Home', url: '/' },
-    { menuTitle: 'USA', url: '/countries/usa' },
+    { menuTitle: 'Continents', url: '/continents'},
+    { menuTitle: 'Usa', url: '/countries/usa' },
     { menuTitle: 'Italy', url: '/countries/italy' },
-    { menuTitle: 'Continents', url: '/continents'}
 ]
 
 export default menuItems
