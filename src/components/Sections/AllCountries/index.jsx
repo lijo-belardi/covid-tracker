c3f4724078d3bb712be4db12b47de911ed63b55b
Lijo <lijo.belardi@icloud.com>
2022-06-28 17:26:05 +0200

    styled allCountries's title

--- src/components/Sections/AllCountries/index.jsx
+++ src/components/Sections/AllCountries/index.jsx
@@ -6,11 +6,18 @@ import CountriesTable from '../../Table/CountriesTable'
 import { Container, Typography, Grid } from '@mui/material'
 // Style
 import styles from './index.module.scss'
+// Others import
+import { motion } from 'framer-motion'
 
 const AllCountries = () => {
     return (
-        <Container className={styles.AllCoutriesContainer}>
-            <Typography variant='h3' align='center'>
+        <Container className={styles.AllCountriesContainer}>
+            <Typography
+                variant='h3'
+                align='center'
+                component={motion.div}
+                sx={{ marginBottom: '1rem', backgroundColor: '#1a76d3', color: '#FAF8FF', borderRadius: '0.313rem' }}
+                whileHover={{ backgroundColor: '#FAF8FF', color: '#1a76d3', outline: '1px solid #1a76d3' }}>
                 All Countries
             </Typography>
 
