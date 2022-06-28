12d9bffc3e9b0441959df9a1a9ea44cc40655c11
Lijo <lijo.belardi@icloud.com>
2022-06-28 15:55:14 +0200

    Added comments

--- src/components/Sections/ContinentsSection/index.jsx
+++ src/components/Sections/ContinentsSection/index.jsx
@@ -9,11 +9,14 @@ import { Container, Typography, Grid, Card } from '@mui/material'
 import styles from './index.module.scss'
 // Others import 
 import { motion } from 'framer-motion'
+
 const ContinentsSection = () => {
     return (
         <Container
             className={styles.continentsContainer}
             sx={{ minHeight: '31.25rem' }}>
+
+            {/* Title */}
             <Typography
                 variant='h3'
                 align='center'
@@ -22,16 +25,20 @@ const ContinentsSection = () => {
                 whileHover={{ scale: 0.99, color: '#2196f3' }}>
                 Continents
             </Typography>
+
+
             {/* TODO Link ad ogni continente */}
+            {/* Grid */}
             <Grid container spacing={2}>
+                {/* Grid - Table */}
                 <Grid item xs={12} md={6} lg={4} >
-                    {/* Table */}
                     <Card sx={{ padding: '0.5rem' }}>
                         <ContinentsTable />
                     </Card>
                 </Grid>
+
+                {/* Grid - BarChart */}
                 <Grid item xs={12} md={6} lg={8}>
-                    {/*  BarChart */}
                     <Card sx={{ minHeight: '100%', padding: '0.5rem' }}>
                         <ContinentsBarChart />
                     </Card>
