9886cc08679d73492c2d26b8c23c808e52f7889f
Lijo <lijo.belardi@icloud.com>
2022-06-29 15:01:41 +0200

    Added description card

--- src/components/Sections/AllCountries/index.jsx
+++ src/components/Sections/AllCountries/index.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import CountriesTable from '../../Table/CountriesTable'
 import Title from '../../Title'
 // Mui components
-import { Container, Typography, Grid } from '@mui/material'
+import { Container, Typography, Card } from '@mui/material'
 // Style
 import styles from './index.module.scss'
 // Others import
@@ -14,8 +14,22 @@ const AllCountries = () => {
     return (
         <Container className={styles.AllCountriesContainer}>
             <Title text='All Countries' />
-
-            <CountriesTable />
+            <Card
+                sx={{ padding: '1rem' }}
+                component={motion.div}
+                whileHover={{ outline: '1px solid #1a76d3' }}>
+                <Typography variant='h5'>Description</Typography>
+                <Typography align='justify'>
+                    In this section, it is possible to view a table with all the world states, with the relative number of cases and deaths.
+                    By clicking on the name of the individual state, you can get more details on the dedicated page.
+                </Typography>
+            </Card>
+            <Card
+                sx={{ marginTop: '1rem' }}
+                component={motion.div}
+                whileHover={{ outline: '1px solid #1a76d3' }}>
+                <CountriesTable />
+            </Card>
         </Container>
     )
 }
