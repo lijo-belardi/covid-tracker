eab50a533b54d18dd8030b6409ca7ce967c297f4
Lijo <lijo.belardi@icloud.com>
2022-06-30 16:57:14 +0200

    Added Subtitle

--- src/components/Sections/AllCountries/index.jsx
+++ src/components/Sections/AllCountries/index.jsx
@@ -9,21 +9,18 @@ import { Container, Typography, Card } from '@mui/material'
 import styles from './index.module.scss'
 // Others import
 import { motion } from 'framer-motion'
+import Subtitle from '../../Subtitle'
 
 const AllCountries = () => {
     return (
         <Container className={styles.AllCountriesContainer}>
             <Title text='All Countries' />
-            <Card
-                sx={{ padding: '1rem' }}
-                component={motion.div}
-                whileHover={{ outline: '1px solid #1a76d3' }}>
-                <Typography variant='h5'>Description</Typography>
-                <Typography align='justify'>
-                    In this section, it is possible to view a table with all the world states, with the relative number of cases and deaths.
-                    By clicking on the name of the individual state, you can get more details on the dedicated page.
-                </Typography>
-            </Card>
+            <Subtitle
+                showDetails={true}
+                description="In this section, it is possible to view a table with all the world states, 
+                with the relative number of cases and deaths."
+                details="By clicking on the name of the individual state, 
+                you can get more details on the dedicated page."/>
             <Card
                 sx={{ marginTop: '1rem' }}
                 component={motion.div}
