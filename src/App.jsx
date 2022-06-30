7fd73f66fc0a3dfe303789399200b95541c080c2
Lijo <lijo.belardi@icloud.com>
2022-06-30 12:56:51 +0200

    Deleted Italy's page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 import Continents from './pages/Continents';
 import Country from './pages/Country';
 import USAPage from './pages/USA';
-import ItalyPage from './pages/Italy';
+
 import UsaStatePage from './pages/UsaState';
 
 // Contexts
@@ -19,8 +19,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/continents' element={<Continents />} />
-            {/* TODO Route per i singoli continent continenti */}
-            <Route path='/countries/italy' element={<ItalyPage />} />
+            {/* TODO Route per i singoli continent continenti */}          
             <Route path='/countries/usa' element={<USAPage />} />
             <Route path='/countries/usa/:state' element={<UsaStatePage />} />
             <Route path='/countries/:country' element={<Country />} />
