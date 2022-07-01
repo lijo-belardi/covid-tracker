9c50f0930c30438d5701aa5db2b07f7b95e76e99
Lijo <lijo.belardi@icloud.com>
2022-07-01 20:44:59 +0200

    improved code's readability

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -8,7 +8,13 @@ const Title = (props) => {
         <Typography
             variant='h3'
             align='center'
-            sx={{ marginBottom: '1rem', backgroundColor: '#1a76d3', color: '#FAF8FF', borderRadius: '0.313rem', padding: '0.5rem' }}>
+            sx={{
+                marginBottom: '1rem',
+                backgroundColor: '#1a76d3',
+                color: '#FAF8FF',
+                borderRadius: '0.313rem',
+                padding: '0.5rem'
+            }}>
             {props.text}
         </Typography>
     )
